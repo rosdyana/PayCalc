451422a3625a89217faff026efe53c186243a7c0
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2016-10-30 18:09:40 +0800

    ! Initial commit to github - moving from my local repository

---
+++ joomla_module/mod_payrollcalc.php
@@ -0,0 +1,15 @@
+<?php
+defined('_JEXEC') or die('Direct Access to this location is not allowed.');
+
+/**
+ * @version 1.0.0
+ * @package PayrollCalculator 
+ * @copyright 2016 RosdyanaKusuma
+ * @author 2016 RosdyanaKusuma(rosdyana.kusuma@mail.com)
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @description Payroll Calculator
+ * Homepage: http://r3m1ck.us/
+*/
+
+   require JModuleHelper::getLayoutPath('mod_payrollcalc');
+?>
