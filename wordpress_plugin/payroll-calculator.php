b094809cd56851e320da40026e96d1bc9ff12022
Rosdyana Kusuma <rosdyana.kusuma@gmail.com>
2016-11-21 17:35:11 +0800

    + added wordpress plugin !will be submitted soon at wordpress.org

---
+++ wordpress_plugin/payroll-calculator.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+	Plugin Name	: Indonesian Payroll Calculator
+	Plugin URI	: -
+	Description	: a plugin to show ads of adsense, chitika ,etc then you can put it into anywhere in your posting as you wish :) 
+	Version		: 0.0.1
+	Author		: Rosdyana Kusuma
+	Author URI	: http://r3m1ck.us/about
+	License		: GPL2
+*/
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-plugin-name-activator.php
+ */
+function activate_plugin_name() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-activator.php';
+	Plugin_Name_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-plugin-name-deactivator.php
+ */
+function deactivate_plugin_name() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name-deactivator.php';
+	Plugin_Name_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_plugin_name' );
+register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-plugin-name.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_plugin_name() {
+
+	$plugin = new Plugin_Name();
+	$plugin->run();
+
+}
+run_plugin_name();
